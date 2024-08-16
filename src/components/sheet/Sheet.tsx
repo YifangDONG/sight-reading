410160888b6256bfef04c010933921d58256f5d0
samantha12dong <samantha12dong@gmail.com>
2024-08-16 22:42:14 +0000

    extract parameter for create sheet

--- src/components/sheet/Sheet.tsx
+++ src/components/sheet/Sheet.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-import Vex from 'vexflow'
-const { Renderer, Stave, StaveNote, Voice, Formatter } = Vex.Flow
+import Vex, { StaveNote } from 'vexflow'
+const { Renderer, Stave, Voice, Formatter } = Vex.Flow
 const staveMargin = 10
 const Sheet = () => {
   const [containerWidth, setContainerWidth] = useState(window.innerWidth)
@@ -10,70 +10,83 @@ const Sheet = () => {
       setContainerWidth(window.innerWidth)
     }
     window.addEventListener('resize', handleResize)
-    drawMusic()
+    drawMusic({
+      beatNumbers: 4,
+      beatValues: 4,
+      trebleNotes: [
+        new StaveNote({
+          clef: 'treble',
+          keys: ['c/4', 'e/4', 'f#/4', 'd#/5'],
+          duration: 'w'
+        })
+      ],
+      bassNotes: [
+        new StaveNote({
+          clef: 'bass',
+          keys: ['ab/2', 'd/3', 'f/3', 'bb/3'],
+          duration: 'w'
+        })
+      ]
+    })
     return () => window.removeEventListener('resize', handleResize)
   }, [containerWidth])
   return <div id="sheet-container" className="w-full max-w-full mx-auto md:max-w-screen-md lg:max-w-screen-lg"></div>
 }
 
 export default Sheet
-const drawMusic = () => {
+
+type SheetParams = {
+  beatNumbers: number
+  beatValues: number
+  trebleNotes?: StaveNote[]
+  bassNotes?: StaveNote[]
+}
+
+const drawMusic = (sheetParams: SheetParams) => {
   const container = document.getElementById('sheet-container') as HTMLDivElement
-  container.innerHTML = '' // TODO if remove this line, the stave is repeat twice
+  container.innerHTML = ''
   const renderer = new Renderer(container, Renderer.Backends.SVG)
   const width = container.offsetWidth
   const height = 500
   renderer.resize(width, height)
   const context = renderer.getContext()
 
-  var staveTreble = new Stave(staveMargin, 40, width - 2 * staveMargin)
-  staveTreble.addClef('treble').addTimeSignature('4/4')
-  staveTreble.setContext(context).draw()
+  const formatter = new Vex.Flow.Formatter()
 
-  const bassStaveY = staveTreble.getYForLine(5)
+  const timeSignature = `${sheetParams.beatNumbers}/${sheetParams.beatValues}`
 
-  const staveBass = new Stave(staveMargin, bassStaveY, width - 2 * staveMargin)
-  staveBass.addClef('bass').addTimeSignature('4/4')
+  var staveTreble
+  if (sheetParams.trebleNotes) {
+    staveTreble = new Stave(staveMargin, 40, width - 2 * staveMargin)
+    staveTreble.addClef('treble').addTimeSignature(timeSignature)
+    staveTreble.setContext(context).draw()
 
-  staveBass.setContext(context).draw()
-  console.log('width : ' + width)
+    const voiceTreble = new Vex.Flow.Voice({
+      num_beats: sheetParams.beatNumbers,
+      beat_value: sheetParams.beatValues,
+      resolution: Vex.Flow.RESOLUTION
+    })
+    voiceTreble.addTickables(sheetParams.trebleNotes).setStave(staveTreble)
+    formatter.joinVoices([voiceTreble])
+    formatter.format([voiceTreble], 4)
+    voiceTreble.setContext(context).draw()
+  }
 
-  const notesTreble = new StaveNote({
-    clef: 'treble',
-    keys: ['c/4', 'e/4', 'f#/4', 'd#/5'],
-    duration: 'w'
-  })
-  const notesBass = new StaveNote({
-    clef: 'bass',
-    keys: ['ab/2', 'd/3', 'f/3', 'bb/3'],
-    duration: 'w'
-  })
-  var voiceTreble = new Vex.Flow.Voice({
-    num_beats: 4,
-    beat_value: 4,
-    resolution: Vex.Flow.RESOLUTION
-  })
-  var voiceBass = new Vex.Flow.Voice({
-    num_beats: 4,
-    beat_value: 4,
-    resolution: Vex.Flow.RESOLUTION
-  })
+  var staveBass
+  if (sheetParams.bassNotes) {
+    const bassStaveY = staveTreble ? staveTreble.getYForLine(5) : 40
+    staveBass = new Stave(staveMargin, bassStaveY, width - 2 * staveMargin)
+    staveBass.addClef('bass').addTimeSignature(timeSignature)
+    staveBass.setContext(context).draw()
 
-  voiceTreble.addTickables([notesTreble]).setStave(staveTreble)
-  voiceBass.addTickables([notesBass]).setStave(staveBass)
-
-  var formatter = new Vex.Flow.Formatter()
-
-  // Make sure the staves have the same starting point for notes
-  var startX = Math.max(staveTreble.getNoteStartX(), staveBass.getNoteStartX())
-  staveTreble.setNoteStartX(startX)
-  staveBass.setNoteStartX(startX)
-
-  // the treble and bass are joined independently but formatted together
-  formatter.joinVoices([voiceTreble])
-  formatter.joinVoices([voiceBass])
-  formatter.format([voiceTreble, voiceBass], staveTreble.getWidth() - startX)
-
-  voiceTreble.setContext(context).draw()
-  voiceBass.setContext(context).draw()
+    var voiceBass = new Vex.Flow.Voice({
+      num_beats: sheetParams.beatNumbers,
+      beat_value: sheetParams.beatValues,
+      resolution: Vex.Flow.RESOLUTION
+    })
+    voiceBass.addTickables(sheetParams.bassNotes).setStave(staveBass)
+    formatter.joinVoices([voiceBass])
+    formatter.format([voiceBass], 4)
+    voiceBass.setContext(context).draw()
+  }
 }
